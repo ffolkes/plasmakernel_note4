cf1738564e544b1754e992a7aa961e80004911be
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-21 15:39:47 -0400

    Import build tools, Fix missing Mac support

---
+++ buildimg/img.sh
@@ -0,0 +1,2 @@
+./mkbootfs boot.img-ramdisk | gzip > newramdisk.cpio.gz
+./mkbootimg --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
