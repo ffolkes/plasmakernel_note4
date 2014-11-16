0f9931f515268874a74a10a8afbe7dafe0544c06
ffolkes <ffolkes@ffolkes.com>
2014-11-16 03:38:40 -0500

    Revert "board: add persistent ram headers and tables"
    This reverts commit 177bf47380a6f304be8e039c65b1af30693cb447.
    
    Build fails with:
    arch/arm/mach-msm/built-in.o: In function `apq8084_reserve':
    :(.init.text+0x1d2c): undefined reference to `msm_reserve'

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -46,32 +46,12 @@
 #include "clock.h"
 #include "platsmp.h"
 
-static struct memtype_reserve msm8974_reserve_table[] __initdata = {
-    [MEMTYPE_SMI] = {
-    },
-    [MEMTYPE_EBI0] = {
-        .flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-    },
-    [MEMTYPE_EBI1] = {
-        .flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-    },
-};
-
-static int msm8974_paddr_to_memtype(phys_addr_t paddr)
-{
-    return MEMTYPE_EBI1;
-}
-
-static struct reserve_info msm8974_reserve_info __initdata = {
-    .memtype_reserve_table = msm8974_reserve_table,
-    .paddr_to_memtype = msm8974_paddr_to_memtype,
-};
+#ifdef CONFIG_ANDROID_PERSISTENT_RAM
 
 #define PERSISTENT_RAM_BASE 0xbff00000
 #define PERSISTENT_RAM_SIZE SZ_1M
 #define RAM_CONSOLE_SIZE (124*SZ_1K * 2)
 
-#ifdef CONFIG_ANDROID_PERSISTENT_RAM
 static struct persistent_ram_descriptor pram_descs[] = {
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
         {
